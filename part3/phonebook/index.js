5599355d5e50309313ad8baf6adb57224b3202d4
sfgarcia <sfgarcia@uc.cl>
2023-08-08 19:27:18 -0400

    Step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,13 +28,24 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === Number(id))
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>
-        `
-    )
-  })
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `
+  )
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
