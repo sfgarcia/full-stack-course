8cefa8db4fb657978fb0ef7fec5f9a1d64d8368a
sfgarcia <sfgarcia@uc.cl>
2023-08-08 20:12:15 -0400

    Step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('request', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens['request'](req, res)
+    ].join(' ')
+  }
+)
+)
 
 let persons = [
     { 
