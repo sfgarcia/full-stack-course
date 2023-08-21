cfa7675c3ee6044b3f37701f8cfe5c2bd29deb8d
sfgarcia <sfgarcia@uc.cl>
2023-08-20 20:56:35 -0400

    Set db

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -11,9 +12,18 @@ const blogSchema = new mongoose.Schema({
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
+const url = process.env.MONGODB_URI
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+console.log('connecting to', url)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
@@ -36,7 +46,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
